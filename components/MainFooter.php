7ec672a39b9729a36b9b49dca6557d89120041f0
Humberto <humbertobarreto1213@gmail.com>
2019-01-16 20:06:28 -0200

    Componentes de página
    Rodapé e cabeçalho que acompanham todas páginas do sistema.

---
+++ components/MainFooter.php
@@ -0,0 +1,7 @@
+<footer class="page-footer">
+<p>Todos os direitos reservados as intituições responsáveis pela idealização e produção do projeto.</p>
+  <div class="footer-image-container">
+    <img src="./images/brasao-sp.png" alt="Brasão SP">
+    <img src="./images/cps-logo.jpg" alt="Imagem Centro Paula Souza">
+  </div>
+</footer>
---
+++ components/MainHeader.php
@@ -0,0 +1,21 @@
+<header class="page-header">
+  <nav class="nav-login">
+    <a href="./login.php">
+      <button class="btn btn-login">
+        <i class="fa fa-user"></i> Entrar
+      </button>
+    </a>
+    <div style="clear: both"></div>
+  </nav>
+  <div class="page-banner">
+    <a href="./">
+      <img src="./images/logo.png" class="page-banner-logo">
+    </a>
+    <h3>Cadastro de <br> Projetos Acadêmicos</h3>
+  </div>
+</header>
+<nav class="page-navbar">
+  <a href="./">Início</a>
+  <a href="#">Acervo</a>
+  <a href="#">Contatos</a>
+</nav>
