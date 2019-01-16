c9f80d35b4d8d4138895f65c55342346c5c94b7d
Humberto <humbertobarreto1213@gmail.com>
2019-01-16 19:34:44 -0200

    Criação da página inicial
    Pagina inicial que armazena informações sobre o sistema.

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#656565">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Início • CPA</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./css/master.css">
+    <link rel="stylesheet" media="(max-width: 680px)" href="./css/responsive.css">
+
+    <!-- Font Awesome -->
+    <script src="https://use.fontawesome.com/43f21fbb10.js"></script>
+  </head>
+  <body class="home-container">
+
+    <?php require_once('./components/MainHeader.php') ?>
+
+    <main class="content-container">
+      <header class="content-header">
+        <h1>O que é o Cadastro de Projetos Acadêmicos?</h1>
+      </header>
+      <article class="content-body content-home">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eveniet accusantium aliquid fugiat, asperiores quae et quo aperiam quam ab esse, vero nulla ratione quod, animi aspernatur voluptates ipsum pariatur?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum tenetur vitae quam provident. Aliquam, nulla quos illo aut nisi accusamus vero inventore hic tempora quasi omnis pariatur minima corporis! Quam!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed molestiae, magnam rerum voluptate nesciunt recusandae minima cumque nobis. Architecto ducimus nihil minima minus voluptatem, voluptates recusandae quod repellendus facilis animi!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed molestiae, magnam rerum voluptate nesciunt recusandae minima cumque nobis. Architecto ducimus nihil minima minus voluptatem, voluptates recusandae quod repellendus facilis animi!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed molestiae, magnam rerum voluptate nesciunt recusandae minima cumque nobis. Architecto ducimus nihil minima minus voluptatem, voluptates recusandae quod repellendus facilis animi!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed molestiae, magnam rerum voluptate nesciunt recusandae minima cumque nobis. Architecto ducimus nihil minima minus voluptatem, voluptates recusandae quod repellendus facilis animi!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed molestiae, magnam rerum voluptate nesciunt recusandae minima cumque nobis. Architecto ducimus nihil minima minus voluptatem, voluptates recusandae quod repellendus facilis animi!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed molestiae, magnam rerum voluptate nesciunt recusandae minima cumque nobis. Architecto ducimus nihil minima minus voluptatem, voluptates recusandae quod repellendus facilis animi!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed molestiae, magnam rerum voluptate nesciunt recusandae minima cumque nobis. Architecto ducimus nihil minima minus voluptatem, voluptates recusandae quod repellendus facilis animi!</p>
+        <div class="image-container">
+          <img src="https://www.kingengenharia.com.br/sites/default/files/imagens-produto/im.-dest.-projeto.jpg">
+          <img src="https://www.kingengenharia.com.br/sites/default/files/imagens-produto/im.-dest.-projeto.jpg">
+          <img src="https://www.kingengenharia.com.br/sites/default/files/imagens-produto/im.-dest.-projeto.jpg">
+        </div>
+      </article>
+
+    </main>
+
+    <?php require_once('./components/MainFooter.php') ?>
+
+  </body>
+</html>
