b6898db53ab2b5dfa7c6a4d1413154f1aab276ae
Humberto <humbertobarreto1213@gmail.com>
2019-01-16 19:39:21 -0200

    Ação PHP de login
    Os dados serão validados nesta página com base na associação ao banco de dados.

---
+++ loga.php
@@ -0,0 +1,31 @@
+<?php
+include("config.php");
+session_start();
+$_SESSION["nome"] = $_POST["login"];
+$_SESSION["senha"] = $_POST["senha"];
+$_SESSION["tipo"] = $_POST["tipo"];
+
+$pesquisausu=$conn->prepare("SELECT * FROM usuarios 
+WHERE (loginUsuario = :loginUsuario) AND (tipoUsuario = :tipoUsuario)");
+
+$pesquisausu->bindParam(':loginUsuario', $_SESSION['nome']);
+$pesquisausu->bindParam(':tipoUsuario', $_SESSION['tipo']);
+
+$pesquisausu->execute();
+
+$usu=$pesquisausu->fetch(PDO::FETCH_ASSOC);
+
+if (($_SESSION["nome"]==$usu['loginUsuario'])&&($_SESSION["senha"]==$usu['senhaUsuario'])&&($_SESSION["tipo"]=="aluno")) {
+	unset($_SESSION["acesso-negado"]);
+}else if(($_SESSION["nome"]==$usu['loginUsuario'])&&($_SESSION["senha"]==$usu['senhaUsuario'])&&($_SESSION["tipo"]=="professor")) {
+	unset($_SESSION["acesso-negado"]);
+	header('Location: leitura.php');
+} else {
+	unset($_SESSION["nome"]);
+	unset($_SESSION["senha"]);
+	unset($_SESSION["tipo"]);
+	$_SESSION["acesso-negado"] = true;
+	header('Location: login.php');
+}
+
+?>
